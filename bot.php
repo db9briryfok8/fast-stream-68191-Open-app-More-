d45ea32825442716422e65f222406efb5ae29e2a
db9briryfok8 <db9briryfok8@gmail.com>
2017-06-18 13:08:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -26,11 +26,7 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
-			if ($text == "help"){		
-				$messages = [
-				'type' => 'text',
-				'text' => "Function 1 : staus"
-			];
+			
 			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
